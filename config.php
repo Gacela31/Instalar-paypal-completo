4a7bbf1448d09873284c8398fb994fb0f190e201
unknown <gimena.sanchezvil@gmail.com>
2020-10-21 07:16:22 -0300

    finalizado

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('URL_SITIO', 'http://localhost/paypal/');
 
 $apiContext = new \PayPal\Rest\ApiContext(
     new \PayPal\Auth\OAuthTokenCredential(
-        //ClienteID
+        //ClienteId
         'AafC0UbbDXLc8VewwifTwLIJ0jWuB2wH_RsJY-t1xuFdW-_jl03VKd0Kt-rwvUS2tLeq4r7alu901fIv',
         //Secret
         'ENsHPAE3I9GwaxQNG1edizw_scWs5ag1vKmKD8EXrhdr4uGNgPQ9Ld6uzx-trRS7ijcoFbXwgBTQ634w'
